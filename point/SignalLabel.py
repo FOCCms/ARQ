fa5bf3bc03a1a1963ed4221146fc26730450052b
BaconNot <37242177+BaconNot@users.noreply.github.com>
2018-04-13 17:56:23 +0300

    SignalLabel added

---
+++ point/SignalLabel.py
@@ -0,0 +1,6 @@
+from point.Label import Label
+
+class SignalLabel(Label):
+
+    def getLabel(self):
+        
--- point/TextDescription.py
+++ point/TextDescription.py
@@ -3,7 +3,7 @@ from point.PointDescription import PointDescription
 
 class TextDescription(PointDescription):
     """Текстовое описание точки"""
-    __text: str
+    __text = str()
 
     def setDescriptionData(self, data):
         self.__text = str(data)
